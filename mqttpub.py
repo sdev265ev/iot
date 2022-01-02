891db3097c3136e54bc189cc0fda14400afdc6f9
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2022-01-02 06:25:57 -0600

    Update mqttpub.py

--- mqttpub.py
+++ mqttpub.py
@@ -1,31 +1,9 @@
-import paho.mqtt.client as mqtt #import the client1
-import time
-############
-def on_message(client, userdata, message):
-        print("message received " ,str(message.payload.decode("utf-8")))
-        print("message topic=",message.topic)
-        print("message qos=",message.qos)
-        print("message retain flag=",message.retain)
-########################################
-broker_address="192.168.254.55"
-#broker_address="test.mosquitto.org"
-print("creating new instance")
-client = mqtt.Client() #create new instance
-client.username_pw_set("mqttuser", "mqttpass")
-client.on_message=on_message #attach function to callback
-
-print("connecting to broker")
-client.connect(broker_address) #connect to broker
-client.loop_start() #start the loop
-print("Subscribing to topic","test/status")
-client.subscribe("test/status")
-
-for x in range(20):
-        #print("Publishing message to topic","test/status")
-        #client.publish("test/status", "Hello World")
-        #client.publish("test/status","on")
-
-        time.sleep(4) # wait
-#client.loop_stop() #stop the loop
-
-
+import paho.mqtt.client as mqtt #import the client
+def Pubmqtt(topic, message):
+	broker_address="192.168.254.55"
+	#broker_address="test.mosquitto.org"
+	client = mqtt.Client()
+	client.username_pw_set("mqttuser", "mqttpass")
+	client.connect(broker_address)
+	client.publish(topic, message)
+	client1.disconnect()
