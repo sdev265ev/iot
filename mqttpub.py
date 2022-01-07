fda5de6d1bc9fae7cd53538fca9e81b9194f3da4
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2022-01-07 12:19:22 -0600

    Update mqttpub.py

--- mqttpub.py
+++ mqttpub.py
@@ -1,9 +1,10 @@
 import paho.mqtt.client as mqtt #import the client
-def Pubmqtt(topic, message):
-	broker_address="192.168.254.55"
-	#broker_address="test.mosquitto.org"
+def Pubmqtt(topic, message, qos=0, retain=True):
+	brokerAddress="192.168.254.55"
+	#brokerAddress="test.mosquitto.org"
+	port=1883
 	client = mqtt.Client()
 	client.username_pw_set("mqttuser", "mqttpass")
-	client.connect(broker_address)
-	client.publish(topic, message)
+	client.connect(brokerAddress, port)
+	client.publish(topic, message, qos, retain)
 	client1.disconnect()
