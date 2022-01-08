ef9834458fb73c6c986adef02407a9d09012fc6f
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2022-01-07 18:39:52 -0600

    Update main.py

--- main.py
+++ main.py
@@ -31,16 +31,17 @@ try:
 	print ()
 	print ()
 	time.sleep(2)
+	offset =  1.236
 	while(1):
 		ADC_Value = ADC.ADS1256_GetAll()
-		print ("0 ADC = %lf"%(ADC_Value[0]*5.0/0x7fffff))
-		print ("1 ADC = %lf"%(ADC_Value[1]*5.0/0x7fffff))
-		print ("2 ADC = %lf"%(ADC_Value[2]*5.0/0x7fffff))
-		print ("3 ADC = %lf"%(ADC_Value[3]*5.0/0x7fffff))
-		print ("4 ADC = %lf"%(ADC_Value[4]*5.0/0x7fffff))
-		print ("5 ADC = %lf"%(ADC_Value[5]*5.0/0x7fffff))
-		print ("6 ADC = %lf"%(ADC_Value[6]*5.0/0x7fffff))
-		print ("7 ADC = %lf"%(ADC_Value[7]*5.0/0x7fffff))
+		print ("0 ADC = %lf"%(ADC_Value[0]*5.0/0x7fffff - offset))
+		print ("1 ADC = %lf"%(ADC_Value[1]*5.0/0x7fffff - offset))
+		print ("2 ADC = %lf"%(ADC_Value[2]*5.0/0x7fffff - offset))
+		print ("3 ADC = %lf"%(ADC_Value[3]*5.0/0x7fffff - offset))
+		print ("4 ADC = %lf"%(ADC_Value[4]*5.0/0x7fffff - offset))
+		print ("5 ADC = %lf"%(ADC_Value[5]*5.0/0x7fffff - offset))
+		print ("6 ADC = %lf"%(ADC_Value[6]*5.0/0x7fffff - offset))
+		print ("7 ADC = %lf"%(ADC_Value[7]*5.0/0x7fffff - offset))
 		print ("\33[9A")
         
 except :
