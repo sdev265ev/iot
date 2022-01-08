20e2c14a351df7655229ab7902e7c4be2cfe8d7e
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2022-01-07 18:52:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ try:
 	print ()
 	print ()
 	time.sleep(2)
-	offset =  1.2345
+	offset =  1.235
 	while(1):
 		ADC_Value = ADC.ADS1256_GetAll()
 		print ("0 ADC = %lf"%(ADC_Value[0]*5.0/0x7fffff - offset))
